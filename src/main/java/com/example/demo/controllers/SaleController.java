77fb628f92ebe4046b299a7ea09c0abd8c1fd0f0
JhonJaimeF <jhonfredyjaime@gmail.com>
2024-11-28 20:39:32 -0500

    update

--- src/main/java/com/example/demo/controllers/SaleController.java
+++ src/main/java/com/example/demo/controllers/SaleController.java
@@ -3,22 +3,22 @@ package com.example.demo.controllers;
 import com.example.demo.entyties.Sale;
 import com.example.demo.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/sales") public class SaleController {
     @Autowired
-    private SaleService saleService; @PostMapping
+    private SaleService saleService;
+    @GetMapping
+    public List<Sale> getAllSales() {
+        return saleService.readSales();
+        return null;
+    }
+    @PostMapping
     public Sale createSale(@RequestBody Sale sale) {
         return saleService.createSale(sale);
     }
-    public List<Sale> getAllSales() {
-        //return saleService.readSales();
-        return null;
-    }
+
 }
